2b40e6fab8da59af9e15961fb82a0a96a0d0c73c
Carlo Corradini <c.corradini@ad.enginsoft.com>
2020-12-04 19:27:39 +0100

    with url

--- tests/createTestConnection.ts
+++ tests/createTestConnection.ts
@@ -12,10 +12,7 @@ export default async () => {
     entities: [User],
     cache: {
       type: 'ioredis',
-      options: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-      },
+      port: process.env.REDIS_URL,
     },
   });
 };
--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -1,15 +1,1 @@
-export default async () => {
-  process.env.DATABASE_URL = `postgres://${
-    process.env.POSTGRES_USER ? process.env.POSTGRES_USER : 'postgres'
-  }:${process.env.POSTGRES_PASSWORD ? process.env.POSTGRES_PASSWORD : 'password'}@${
-    process.env.POSTGRES_HOST ? process.env.POSTGRES_HOST : 'localhost'
-  }:${process.env.POSTGRES_PORT ? process.env.POSTGRES_PORT : 5432}/${
-    process.env.POSTGRES_DB ? process.env.POSTGRES_DB : 'graphmarket-test'
-  }`;
-
-  process.env.REDIS_URL = `redis://:@${
-    process.env.REDIS_HOST ? process.env.REDIS_HOST : 'localhost'
-  }:${process.env.REDIS_PORT ? process.env.REDIS_PORT : 6379}/1`;
-
-  process.env.JWT_SECRET = 'secret';
-};
+export default async () => {};

594e34d9d22496b686ecb007a9fa6e4e32823756
Carlo Corradini <c.corradini@ad.enginsoft.com>
2020-12-04 03:33:23 +0100

    vediamo

--- tests/createTestConnection.ts
+++ tests/createTestConnection.ts
@@ -2,19 +2,16 @@ import { createConnection, ConnectionOptions } from 'typeorm';
 import { User } from '../src/entities';
 
 export default async () => {
-  console.log(`DATABASE_URL: ${process.env.DATABASE_URL}`);
-  console.log(`REDIS_URL: ${process.env.REIDS_URL}`);
-
   return createConnection(<ConnectionOptions>{
     type: 'postgres',
-    url: process.env.DATABASE_URL,
+    url: 'postgres://postgres:password@localhost:5432/graphmarket-test',
     synchronize: true,
     dropSchema: true,
     logging: false,
     entities: [User],
     cache: {
       type: 'ioredis',
-      port: process.env.REDIS_URL,
+      port: 'redis://:@localhost:6379/1',
     },
   });
 };

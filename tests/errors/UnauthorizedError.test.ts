822e77d91f773d2809b9fb75049e8635abebf7b0
Carlo <carlo.ita98@gmail.com>
2020-12-08 14:17:54 +0100

    Added tests for errors

---
+++ tests/errors/UnauthorizedError.test.ts
@@ -0,0 +1,26 @@
+import UnauthorizedError from '../../src/errors/UnauthorizedError';
+
+const throwUnauthorizedError = () => {
+  throw new UnauthorizedError();
+};
+
+describe('Unauthorized error testing', () => {
+  test('it should create an authentication error', () => {
+    expect(new UnauthorizedError()).toBeDefined();
+  });
+
+  test('it should be an error instance', () => {
+    expect(new UnauthorizedError()).toBeInstanceOf(Error);
+  });
+
+  test('is should be throwable', () => {
+    expect(throwUnauthorizedError).toThrow(UnauthorizedError);
+  });
+
+  test('error name and message should be equal to the provided', () => {
+    expect(() => throwUnauthorizedError()).toThrow({
+      name: 'UnauthorizedError',
+      message: 'Insufficient permissions to execute the procedure',
+    });
+  });
+});

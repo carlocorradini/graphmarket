60a83d701fa7e7ad3fb50528f5652d452ba72d22
Carlo <carlo.ita98@gmail.com>
2020-12-08 21:17:30 +0100

    fix env tests

--- tests/__utils__/env.ts
+++ tests/__utils__/env.ts
@@ -2,27 +2,35 @@ import path from 'path';
 import envalid, { bool, str, url } from 'envalid';
 import _ from 'lodash';
 
-const env = envalid.cleanEnv(
-  process.env,
+const env = _.omit(
   {
-    DATABASE_URL: url(),
-    REDIS_URL: url(),
-    DATABASE_SYNCHRONIZE: bool({ default: true }),
-    DATABASE_DROP_SCHEMA: bool({ default: true }),
-    SERVICE_PHONE_TWILIO_ACCOUNT_SID: str(),
-    SERVICE_PHONE_TWILIO_AUTH_TOKEN: str(),
-    SERVICE_PHONE_TWILIO_VERIFICATION_SID: str(),
+    ...envalid.cleanEnv(
+      process.env,
+      {
+        DATABASE_URL: url(),
+        REDIS_URL: url(),
+        DATABASE_SYNCHRONIZE: bool({ default: true }),
+        DATABASE_DROP_SCHEMA: bool({ default: true }),
+        SERVICE_PHONE_TWILIO_ACCOUNT_SID: str(),
+        SERVICE_PHONE_TWILIO_AUTH_TOKEN: str(),
+        SERVICE_PHONE_TWILIO_VERIFICATION_SID: str(),
+      },
+      {
+        dotEnvPath: path.resolve(process.cwd(), '.env.test'),
+        strict: true,
+      },
+    ),
   },
-  {
-    dotEnvPath: path.resolve(process.cwd(), '.env.test'),
-    strict: true,
-  },
+  'isDev',
+  'isDevelopment',
+  'isProd',
+  'isProduction',
+  'isTest',
 );
 
-// @ts-ignore
 process.env = {
   ...process.env,
-  ..._.omit({ ...env }, 'isDev', 'isDevelopment', 'isProd', 'isProduction', 'isTest'),
+  ...env,
 };
 
 export default env;

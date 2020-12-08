984858666d178685d028adcc49bc93977e95c0b0
Carlo <carlo.ita98@gmail.com>
2020-12-08 17:42:12 +0100

    fix old eslint

--- src/typings/express.d.ts
+++ src/typings/express.d.ts
@@ -1,7 +1,6 @@
 import { IToken } from '@app/types';
 
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   namespace Express {
     export interface Request {
       user?: IToken;

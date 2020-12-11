c1fc44a1f147b2178a58db9d4f71f31367e73882
Carlo <carlo.ita98@gmail.com>
2020-12-11 15:41:03 +0100

    file renaming in lowercase

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export { default as authorizationMiddleware } from './AuthorizationMiddleware';
+export { default as authorizationMiddleware } from './authorizationMiddleware';

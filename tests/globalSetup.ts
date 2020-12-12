4eebdb305c23f5c98ae2fa3105e6ff43839eaddc
Andrea Stedile <andrea.stedile@studenti.unitn.it>
2020-12-12 15:36:25 +0100

    Check whether Redis and database can be connected before tests
    For example, if Redis cannot be connected, the output will be as follows:
    
    Error while connecting to Redis
    Error: connect ECONNREFUSED 127.0.0.1:6379
        at TCPConnectWrap.afterConnect [as oncomplete] (net.js:1146:16)
    npm ERR! Test failed.  See above for more details.
    
    Signed-off-by: Andrea Stedile <andrea.stedile@studenti.unitn.it>

--- tests/globalSetup.ts
+++ tests/globalSetup.ts
@@ -1,5 +1,64 @@
 // --- ALWAYS FIRST
-import '@app/config/env';
+import config from '@app/config';
+import IORedis from 'ioredis';
+import { Connection, ConnectionOptions, createConnection } from 'typeorm';
+
 // --- END
 
-export default async () => {};
+/**
+ * Attempts to connect to Redis, and then disconnects.
+ */
+async function testRedis() {
+  return new Promise((resolve, reject) => {
+    const redis = new IORedis(config.REDIS.URL);
+    redis.on('connect', () => {
+      redis.disconnect();
+      resolve(undefined);
+    });
+    redis.on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+/**
+ * Attempts to connect to the database, and then disconnects.
+ */
+async function testDatabase() {
+  return new Promise((resolve, reject) => {
+    (async () => {
+      try {
+        const conn: Connection = await createConnection(<ConnectionOptions>{
+          type: 'postgres',
+          url: config.DATABASE.URL,
+          synchronize: false,
+          dropSchema: false,
+          logging: false,
+          ssl: false,
+        });
+        await conn.close();
+        resolve(undefined);
+      } catch (e) {
+        reject(e);
+      }
+    })();
+  });
+}
+
+export default async () => {
+  try {
+    await testRedis();
+    console.log('Redis can be connected.');
+  } catch (err) {
+    console.error('Error while connecting to Redis');
+    throw err;
+  }
+
+  try {
+    await testDatabase();
+    console.log('Database can be connected');
+  } catch (err) {
+    console.error('Error while connecting to the database');
+    throw err;
+  }
+};

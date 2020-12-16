484b75fc7702adebf582b375f1f28adb45017fb5
Andrea Stedile <andrea.stedile@studenti.unitn.it>
2020-12-16 15:23:53 +0100

    Refactor the bootstrapping procedure into modules

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,38 @@ import 'reflect-metadata';
 import '@app/config/env';
 // --- END
 import readline from 'readline';
-import config from '@app/config';
 import logger from '@app/logger';
-import Server from '@app/server';
+import {
+  dependencyInjectionModule,
+  databaseModule,
+  appModule,
+  graphqlModule,
+} from '@app/server/modules';
 
 // --- BOOTSTRAP
 (async () => {
   try {
-    await Server.getInstance().start(config.NODE.PORT);
+    logger.info('Hello Graph Market, initiating bootstrap procedure');
+
+    logger.info('Starting dependency injection module...');
+    await dependencyInjectionModule.start();
+    logger.info('Dependency injection module started');
+
+    logger.info('Starting database module...');
+    await databaseModule.start();
+    logger.info('Database module started');
+
+    logger.info('Starting GraphQL module...');
+    await graphqlModule.start();
+    logger.info('GraphQL module started');
+
+    logger.info('Starting app module...');
+    await appModule.start();
+    logger.info('App module started');
+
+    logger.info('* * * GraphQL Market is ready! * * *');
   } catch (error) {
-    logger.error(error);
+    logger.error(`Could not bootstrap Graph Market: ${error.message}`);
     process.exit(1);
   }
 })();
@@ -38,9 +60,32 @@ process.on('SIGINT', async () => {
     logger.warn('SIGINT caught twice, skipping');
     return;
   }
+
+  logger.info('Stopping Graph Market');
   sigintCalled = true;
 
-  await Server.getInstance().stop();
+  logger.info('Stopping Graph Market');
+  sigintCalled = true;
+
+  try {
+    logger.info('Stopping database module...');
+    await databaseModule.stop();
+    logger.info('Database module stopped');
+  } catch (error) {
+    // Log the error and continue anyways
+    logger.error(`Could not properly stop the database module: ${error.message}`);
+  }
+
+  try {
+    logger.info('Stopping app module...');
+    await appModule.stop();
+    logger.info('App module stopped');
+  } catch (error) {
+    logger.error(`Could not stop the server: ${error.message}`);
+  }
+
+  // Whatever happened, we now exit the process
+  logger.info('Bye, cruel world');
   process.exit(0);
 });
 // --- END
---
+++ src/server/modules/index.ts
@@ -0,0 +1,4 @@
+export { default as dependencyInjectionModule } from './dependencyInjectionModule';
+export { default as databaseModule } from './databaseModule';
+export { default as appModule } from './appModule';
+export { default as graphqlModule } from './graphqlModule';

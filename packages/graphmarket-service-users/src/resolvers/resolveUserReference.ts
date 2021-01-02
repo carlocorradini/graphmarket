493c32cb312eb3b458c3cc3a62b1fe7a122271a1
Carlo <carlo.ita98@gmail.com>
2021-01-02 14:47:19 +0100

    fix

--- packages/graphmarket-service-users/src/resolvers/resolveUserReference.ts
+++ packages/graphmarket-service-users/src/resolvers/resolveUserReference.ts
@@ -4,6 +4,8 @@ import { UserService } from '@app/services';
 
 const userService: UserService = Container.get(UserService);
 
-export default async function resolveUserReference(reference: Pick<User, 'id'>): Promise<User> {
-  return userService.readOneOrFail(reference.id);
+export default async function resolveUserReference(
+  reference: Pick<User, 'id'>,
+): Promise<User | undefined> {
+  return userService.readOne(reference.id);
 }

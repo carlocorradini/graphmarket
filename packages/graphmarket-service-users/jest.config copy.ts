646db30c3cfc580c139d486173b62f494c664d2c
Carlo <carlo.ita98@gmail.com>
2021-01-26 12:10:16 +0100

    Fix

--- packages/graphmarket-service-users/jest.config copy.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@jest/types';
-import commonConfig from '../../jest.config.common';
-import jsonPackage from './package.json';
-
-export default {
-  ...commonConfig,
-  name: jsonPackage.name,
-  displayName: jsonPackage.name,
-} as Config.InitialOptions;

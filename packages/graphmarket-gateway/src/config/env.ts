d57de0be08ffeead71fc6619005e2997ce11b41d
Carlo Corradini <carlo.ita98@gmail.com>
2021-02-02 18:43:27 +0100

    Env example (#175)
    * Bump envalid from 6.0.2 to 7.0.0
    
    Bumps [envalid](https://github.com/af/envalid) from 6.0.2 to 7.0.0.
    - [Release notes](https://github.com/af/envalid/releases)
    - [Commits](https://github.com/af/envalid/compare/v6.0.2...v7.0.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump envalid from 6.0.2 to 7.0.0
    
    Bumps [envalid](https://github.com/af/envalid) from 6.0.2 to 7.0.0.
    - [Release notes](https://github.com/af/envalid/releases)
    - [Commits](https://github.com/af/envalid/compare/v6.0.2...v7.0.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Added .env.example
    
    * Updated all dependencies
    
    * Updated versions -> working!
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- packages/graphmarket-gateway/src/config/env.ts
+++ packages/graphmarket-gateway/src/config/env.ts
@@ -1,37 +1,29 @@
-import envalid, { str, port, url } from 'envalid';
-import { buildEnv } from '@graphmarket/helpers';
+import { cleanEnv, str, port, url } from 'envalid';
 import { EnvUtil } from '@graphmarket/utils';
+
+// Load '.env' file.
+EnvUtil.loadDotEnvFile();
 
 /**
  * Environment variables sanitized and immutable.
  */
-const env = buildEnv(
-  envalid.cleanEnv(
-    process.env,
-    {
-      NODE_ENV: str({
-        default: 'production',
-        devDefault: 'development',
-        choices: ['production', 'development', 'test'],
-      }),
-      PORT: port({ devDefault: 8080 }),
-      GRAPHQL_PATH: str({ default: '/graphql' }),
-      REDIS_URL: url(),
-      REDIS_TOKEN_BLACKLIST: str({ default: 'TOKEN_BLACKLIST' }),
-      TOKEN_SECRET: str({ devDefault: 'password' }),
-      TOKEN_ALGORITHM: str({ default: 'HS256' }),
-      SERVICES_AUTHENTICATIONS_URL: url({ devDefault: 'http://localhost:8081/graphql' }),
-      SERVICES_USERS_URL: url({ devDefault: 'http://localhost:8082/graphql' }),
-      SERVICES_PRODUCTS_URL: url({ devDefault: 'http://localhost:8083/graphql' }),
-      SERVICES_INVENTORIES_URL: url({ devDefault: 'http://localhost:8084/graphql' }),
-      SERVICES_PURCHASES_URL: url({ devDefault: 'http://localhost:8085/graphql' }),
-      SERVICES_REVIEWS_URL: url({ devDefault: 'http://localhost:8086/graphql' }),
-    },
-    {
-      strict: true,
-      dotEnvPath: EnvUtil.getDotEnvPath(),
-    },
-  ),
-);
+const env = cleanEnv(process.env, {
+  NODE_ENV: str({
+    default: 'production',
+    devDefault: 'development',
+    choices: ['production', 'development', 'test'],
+  }),
+  PORT: port({ devDefault: 8080 }),
+  GRAPHQL_PATH: str({ default: '/graphql' }),
+  REDIS_URL: url(),
+  TOKEN_SECRET: str({ devDefault: 'password' }),
+  TOKEN_ALGORITHM: str({ default: 'HS256' }),
+  SERVICES_AUTHENTICATIONS_URL: url({ devDefault: 'http://localhost:8081/graphql' }),
+  SERVICES_USERS_URL: url({ devDefault: 'http://localhost:8082/graphql' }),
+  SERVICES_PRODUCTS_URL: url({ devDefault: 'http://localhost:8083/graphql' }),
+  SERVICES_INVENTORIES_URL: url({ devDefault: 'http://localhost:8084/graphql' }),
+  SERVICES_PURCHASES_URL: url({ devDefault: 'http://localhost:8085/graphql' }),
+  SERVICES_REVIEWS_URL: url({ devDefault: 'http://localhost:8086/graphql' }),
+});
 
 export default env;

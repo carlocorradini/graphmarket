3f89e2e2d656e6d8e5419b657b5688e51a601743
Carlo <carlo.ita98@gmail.com>
2021-02-08 17:05:41 +0100

    prod env api url

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { environment as commonEnvironment } from './environment.common';
 export const environment = {
   ...commonEnvironment,
   production: true,
-  apiURI: 'http://graphmarket.hopto.org/graphql',
+  apiURI: 'http://graphmarket.hopto.org:8080/graphql',
 };
